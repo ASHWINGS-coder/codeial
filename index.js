48867efbaf19504530d6ad18b4d0c05cccc228ff
Ash <ashwings1996@gmail.com>
2020-03-06 09:46:26 +0530

    added view home.ejs

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const port = 8000 ;
 // use express router
 app.use('/',require('./routes/index'));
 // set up the view engine
-app.use('view engine','ejs');
+app.set('view engine','ejs');
 app.set('views','./views');
 
 app.listen(port,function(err){
