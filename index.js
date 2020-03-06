ad05843ca7b730715b1056934a419c223ad5edf1
Ash <ashwings1996@gmail.com>
2020-03-06 14:25:20 +0530

    added more static files

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const express = require('express');
 const app = express();
 const port = 8000 ;
+
+app.use(express.static('./assets'))
+
 const expressLayouts = require('express-ejs-layouts');
+
 app.use(expressLayouts);
+// extract style and scripts from sub pages into the layout
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
+
+
 // use express router
 app.use('/',require('./routes/index'));
 // set up the view engine
