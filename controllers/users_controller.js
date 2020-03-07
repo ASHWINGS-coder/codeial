17759c7c4340485a6aa9b03190ec357a2a202d9c
Ash <ashwings1996@gmail.com>
2020-03-07 17:40:04 +0530

    passing user data to views

--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -7,6 +7,10 @@ module.exports.profile = function(req,res){
 
 // Render the sign up page
 module.exports.signUp = function(req,res){
+    if(req.isAuthenticated()){
+      return  res.redirect('/users/profile');
+    }
+
     return  res.render('user_sign_up',{
         title:"Codeial | Sign Up"
     })
@@ -14,6 +18,10 @@ module.exports.signUp = function(req,res){
 
 // Render the sign in page
 module.exports.signIn = function(req,res){
+
+    if(req.isAuthenticated()){
+       return res.redirect('/users/profile');
+    }
     return res.render('user_sign_in',{
         title:"Codeial | Sign In"
     })
