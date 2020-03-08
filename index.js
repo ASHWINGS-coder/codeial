f5c6461dd1057de3694a1d90ba27e5d3b61507a4
Ash <ashwings1996@gmail.com>
2020-03-08 17:09:42 +0530

    added post to page

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cookieParser = require('cookie-parser')
 const app = express();
-const port = 8000 ;
+const port = 8001 ;
 const db = require('./config/mongoose');
 
 // used for session cookie
