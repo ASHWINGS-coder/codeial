bcb0a4efb9440d91bbd0970c9cb5f36dca78690d
Ash <ashwings1996@gmail.com>
2020-03-09 12:25:59 +0530

    displayed the post and prepopulated the user and displayed the user who posted the commet

--- controllers/home_controllers.js
+++ controllers/home_controllers.js
@@ -4,10 +4,17 @@ module.exports.home = function(req,res){
     // console.log(req.cookies);
     // res.cookie('user_id',25);
 
-    Post.find({},function(err,posts){
+    // Post.find({},function(err,posts){
+    //     return res.render('home',{
+    //         title:  "Codeial | Home",
+    //         posts: posts
+    //     });
+    // });
+    // populate the user for each post
+    Post.find({}).populate('user').exec(function(err,posts){
         return res.render('home',{
             title:  "Codeial | Home",
             posts: posts
         });
-    });
+    })
 }
