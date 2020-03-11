74a51f3f9368671d05b9af524130b5dd1a98649c
Ash <ashwings1996@gmail.com>
2020-03-11 13:18:58 +0530

    added more notifications

--- controllers/posts_controller.js
+++ controllers/posts_controller.js
@@ -7,9 +7,10 @@ module.exports.create = async function(req,res){
       content: req.body.content,
       user: req.user._id
   });
+  req.flash('success','Post published!');
   return res.redirect('back');
   }catch(err){
-    console.log('Error',err);
+    req.flash('error',err);
     return;
   }
   
@@ -24,14 +25,17 @@ module.exports.destroy =  async  function(req,res){
       post.remove();
   // deleting comments of the particular post 
       await Comment.deleteMany({post : req.params.id})
+
+      req.flash('success','Post and associated comments deleted');
         return res.redirect('back');   
    }else{
+    req.flash('error','You cannot delete this post');
     return res.redirect('back');        
    }
 
   }catch(err){
-    console.log('Error',err);
-    return;
+    req.flash('error',err);
+    return res.redirect('back');    
   }
 
 }
