f66545759da2c9279be62cadb27db510ba3b4447
Ash <ashwings1996@gmail.com>
2020-03-12 11:26:57 +0530

    disp post ajax

--- assets/js/home_posts.js
+++ assets/js/home_posts.js
@@ -11,7 +11,8 @@
                 url : '/posts/create',
                 data : newPostForm.serialize(),
                 success : function(data){
-                    console.log(data);
+                   let newPost = newPostDom(data.data.post);
+                   $('#posts-list-container > ul').prepend( newPost);
                 },error: function(error){
                     console.log(error.responseText);
                 }
@@ -22,6 +23,38 @@
     }
 
     // method to create a post in DOM
+
+    let newPostDom = function(post){
+        return $(`<li id="post-${post._id}">
+        <p>
+                        <small>
+                                <a class="delete-post-button"  href="/posts/destroy/${post._id}"> X </a>
+                        </small>
+                        
+              
+                        ${ post.content }
+        <br>
+        <small>
+        ${ post.user.name }
+        </small>
+        </p>
+        <div class="post-comments">
+       
+                <form action="/comments/create" method="POST">
+                <input type="text" name="content" placeholder="type here to add comment ...">
+                <input type="hidden" name="post" value="${ post._id }">
+                <input type="submit" value="Add Comment">
+                </form>
+           
+        <!--  posting comments list  -->
+    <div class="post-comments-list">
+     <ul id="post-comments-${post._id}">
+    
+         </ul>
+                </div>
+        </div>                                              
+         </li>`)
+    }
 
     createPost();
 }
